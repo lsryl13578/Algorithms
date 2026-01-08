e660eb1837f48092a61fc7de327d3e195c7341a0
Sangryeol Lee <tkdfuf1008@gmail.com>
2026-01-08 19:55:25 +0900

    분수찾기 프로그램 실습
    이 프로그램은 백준 1193번: 분수찾기 문제에 대한 코드입니다.
    
    
    문제 개요
    
    이 프로그램은 입력으로 주어진 자연수 x에 대해, 대각선 형태로 나열된 분수 배열에서 x번째에 위치한 분수를 구하는 알고리즘에 대해서 구현한 프로그램입니다.분수들은 각 대각선마다 개수가 1씩 증가하며, 대각선 번호의 홀짝에 따라 분수의 증가 방향이 달라집니다.
    
    
    알고리즘 설명
    
    분수 배열을 직접 생성하지 않고, 대각선 단위로 분수 개수를 누적하는 방식으로 접근합니다.
    
    1. 현재 대각선에 포함된 분수의 개수(count)와
    이전 대각선까지의 분수 총합(hap)을 관리한다.
    
    2. x가 hap + count 범위 안에 들어오는지 검사하여
    x번째 분수가 포함된 대각선을 찾는다.
    
    3. 해당 대각선의 번호가 홀수인지 짝수인지에 따라
    분수의 증가 방향(위 → 아래 / 아래 → 위)을 결정한다.
    
    4. 대각선 내에서의 위치를 계산해 분자와 분모를 구한 뒤 결과를 출력한다.
    
    
    키 포인트
    
    1. 분수 배열을 실제로 생성하지 않아 메모리 사용을 최소화한다.
    
    2. 단순 반복과 누적 합을 이용해 효율적으로 위치를 계산한다.
    
    3. 대각선 번호의 홀짝성을 활용해 분수 방향을 결정한다.
    
    
    요약
    
    이 프로그램은 분수의 규칙적인 대각선 구조를 이용해 수학적 계산만으로 원하는 위치의 분수를 빠르게 찾아내는 알고리즘에 대해서 구현한 프로그램입니다.

---
+++ 분수찾기프로그램.java
@@ -0,0 +1,25 @@
+public class 분수찾기프로그램 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        int x = Integer.parseInt(br.readLine());
+
+        int count = 1; // 대각선 분수 개수
+        int hap = 0; // 이전 대각선까지의 분수 개수 총합
+
+        // x번째 분수가 몇 번째 대각선에 있는지 찾기 위해 계속 대각선을 하나씩 늘려가는 과정
+        while(true) {
+            if (x <= count + hap) { // x번째 분수가 현재 대각선 안에 있는지에 대해 검사
+                if (count % 2 == 1) { // 대각선 번호가 홀수인 경우
+                    System.out.println(count - (x - hap - 1) + "/" + (x - hap)); // 분수가 위 -> 아래 방향으로 증가
+                    break;
+                } else { // 대각선 번호가 짝수인 경우
+                    System.out.println((x - hap) + "/" + (count - (x - hap - 1))); // 분수가 아래 -> 위 방향으로 증가
+                    break;
+                }
+            } else { // 아직 해당 대각선이 아닐 때의 조건문
+                hap += count; // 이전 분수 개수 누적
+                count++; // 다음 대각선으로 이동
+            }
+        }
+    }
+}
